e36afd27bf60027785cd2b27bd97d61c1994f53b
wbs <1134954328@qq.com>
2021-08-20 09:35:19 +0800

    新增路由和页面

---
+++ src/components/NoteDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="note-detail">
+    <h1>{{ msg }}:{{ $route.params.noteId }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: '笔记本详情页'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>

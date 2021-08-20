317fe14c5626879d46722ee22edb2daf62e74402
wbs <1134954328@qq.com>
2021-08-20 14:33:44 +0800

    完成登录/注册页样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="login">
-    <h1>{{ msg }}</h1>
+    <div class="good-overlay">
+      <div class="good-wrapper">
+        <div class="good-container">
+          <div class="main"></div>
+          <div class="form">
+            <h3>创建账户</h3>
+            <div v-show="true" class="register">
+              <input type="text" placeholder="用户名" />
+              <input type="text" placeholder="密码" />
+              <div class="button">创建账号</div>
+            </div>
+            <h3>登录</h3>
+            <div v-show="false" class="login">
+              <input type="text" placeholder="输入用用户名" />
+              <input type="text" placeholder="密码" />
+              <div class="button">登录</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,8 +35,94 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
+<style lang='less' scoped>
+.good-overlay {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+.good-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+.good-container {
+  width: 800px;
+  height: 500px;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgab(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: Helvetica, Arial, sans-serif;
+  display: flex;
+  .main {
+    flex: 1;
+    background: #36bc64
+      url(//cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center
+      no-repeat;
+    background-size: contain;
+  }
+  .form {
+    width: 270px;
+    border-left: 1px solid #ccc;
+    h3 {
+      padding: 10px 20px;
+      font-weight: normal;
+      font-size: 16px;
+      border-top: 1px solid #eee;
+      cursor: pointer;
+      &:nth-of-type(2) {
+        border-bottom: 1px solid #eee;
+      }
+    }
+    .button {
+      background: #2bb964;
+      height: 36px;
+      line-height: 36px;
+      text-align: center;
+      font-weight: bold;
+      color: #fff;
+      border-radius: 4px;
+      margin-top: 18px;
+      cursor: pointer;
+    }
+    .register,
+    .login {
+      padding: 10px 20px;
+      border-top: 1px solid #eee;
+      input {
+        display: block;
+        width: 100%;
+        height: 35px;
+        line-height: 35px;
+        padding: 0 6px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        outline: none;
+        font-size: 14px;
+        margin-top: 10px;
+      }
+      input:focus {
+        border: 3px solid #9dcaf8;
+      }
+      p {
+        font-size: 12px;
+        margin-top: 10px;
+        color: #444;
+      }
+      .error {
+        color: red;
+      }
+    }
+    .login {
+      border-top: 0;
+    }
+  }
 }
 </style>

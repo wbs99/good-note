7799f5740ba9507d166a4909270b61e3cdaa455c
wbs <1134954328@qq.com>
2021-08-24 21:37:05 +0800

    根据 Element ，优化错误提示

--- src/helpers/request.js
+++ src/helpers/request.js
@@ -4,6 +4,7 @@
 // request("/auth/login");
 import axios from "axios";
 import baseURLConfig from "./config-baseURL";
+import { Message } from "element-ui";
 console.log(baseURLConfig);
 
 axios.defaults.headers.post["Content-Type"] =
@@ -31,12 +32,12 @@ export default function request(url, type = "GET", data = {}) {
         if (res.status === 200) {
           resolve(res.data);
         } else {
-          console.error(res.data);
+          Message.error(res.data.msg);
           reject(res.data);
         }
       })
       .catch(err => {
-        console.error({ msg: "网络异常" });
+        Message.error(res.data.msg);
         reject({ msg: "网络异常" });
       });
   });

08322cf2201006b3e08a59765f61fa388dad6c0c
wbs <1134954328@qq.com>
2021-08-25 09:25:57 +0800

    完成笔记本详情页框架

--- src/components/NoteDetail.vue
+++ src/components/NoteDetail.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="note-detail">
-    <h1>{{ msg }}:{{ $route.query.notebookId }}</h1>
-    <h1>{{ msg }}:{{ $route.query.noteId }}</h1>
+  <div id="note"
+       class="detail">
+    <NoteSidebar />
+    <div id="note-detail">
+      <h1>{{ notebookId }}:{{ $route.query.notebookId }}</h1>
+      <h1>{{ noteId }}:{{ $route.query.noteId }}</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import Auth from '../apis/auth'
+import NoteSidebar from './NoteSidebar.vue'
+
 export default {
+  components: {
+    NoteSidebar,
+  },
   data() {
     return {
       msg: 'notebookId',
@@ -24,8 +33,84 @@ export default {
 }
 </script>
 
-<style scoped>
-h1 {
-  color: red;
+<style lang='less' scoped>
+.note-detail {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+
+  .note-detail-ct {
+    height: 100%;
+  }
+
+  .note-empty {
+    font-size: 50px;
+    color: #ccc;
+    text-align: center;
+    margin-top: 100px;
+  }
+
+  .note-bar {
+    padding: 4px 20px;
+    border-bottom: 1px solid #eee;
+    &:after {
+      content: '';
+      display: block;
+      clear: both;
+    }
+    span {
+      font-size: 12px;
+      color: #999;
+      margin-right: 4px;
+    }
+    .iconfont {
+      float: right;
+      margin-left: 4px;
+      font-size: 18px;
+      cursor: pointer;
+    }
+  }
+
+  .note-title {
+    input,
+    span {
+      display: inline-block;
+      width: 100%;
+      border: none;
+      outline: none;
+      font-size: 18px;
+      padding: 10px 20px;
+    }
+  }
+
+  .editor {
+    height: ~'calc(100% - 70px)';
+    position: relative;
+  }
+  textarea,
+  .preview {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+  }
+
+  textarea {
+    border: none;
+    resize: none;
+    outline: none;
+    font-size: 14px;
+    font-family: 'Monaco', courier, monospace;
+  }
+
+  code {
+    color: #f66;
+  }
+}
+#note {
+  display: flex;
+  align-items: stretch;
+  background-color: #fff;
+  flex: 1;
 }
 </style>
---
+++ src/components/NoteSidebar.vue
@@ -0,0 +1,150 @@
+<template >
+  <div class="note-sidebar">
+    <span class="btn add-note">添加笔记</span>
+    <el-dropdown class="notebook-title"
+                 placement='bottom'
+                 @command="handleCommand">
+      <span class="el-dropdown-link">
+        我的笔记本1<i class="iconfont icon-down"></i>
+      </span>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item v-for="notebook in notebooks"
+                          :key="notebook.id"
+                          :command="notebook.id">{{notebook.title}}</el-dropdown-item>
+        <el-dropdown-item command='trash'>回收站</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    <div class="menu">
+      <div>更新事件</div>
+      <div>标题</div>
+    </div>
+    <ul class="notes">
+      <li v-for="note in notes"
+          :key="note.id">
+        <router-link :to="`/note?noteId=${note.id}`">
+          <span class="date">{{note.updateAtFriendly}}</span>
+          <span class="title">{{note.title}}</span>
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      notebooks: [
+        {
+          id: 1,
+          title: 'hello',
+        },
+        {
+          id: 2,
+          title: 'good',
+        },
+      ],
+      notes: [
+        {
+          id: 1,
+          title: '第1个笔记',
+          updateAtFriendly: '刚刚',
+        },
+        {
+          id: 2,
+          title: '第2个笔记',
+          updateAtFriendly: '刚刚',
+        },
+      ],
+    }
+  },
+  methods: {
+    handleCommand(command) {
+      this.$message('click on item ' + command)
+    },
+  },
+}
+</script>
+<style lang="less">
+.note-sidebar {
+  height: 100vh;
+  position: relative;
+  width: 290px;
+  border-right: 1px solid #ccc;
+  background-color: #eee;
+
+  .add-note {
+    position: absolute;
+    right: 5px;
+    top: 12px;
+    color: #666;
+    font-size: 12px;
+    padding: 2px 4px;
+    box-shadow: 0px 0px 2px 0px #ccc;
+    border: none;
+    z-index: 1;
+  }
+
+  .notebook-title {
+    font-size: 18px;
+    font-weight: normal;
+    color: #333;
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    border-bottom: 1px solid #ccc;
+    background-color: #f7f7f7;
+    display: block;
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+  }
+  .el-dropdown-menu__item {
+    width: 200px;
+  }
+
+  .menu {
+    display: flex;
+
+    div {
+      font-size: 12px;
+      padding: 2px 10px;
+      flex: 1;
+      border-right: 1px solid #ccc;
+      border-bottom: 1px solid #ccc;
+
+      &:last-child {
+        border-right: none;
+      }
+    }
+
+    .iconfont {
+      font-size: 10px;
+    }
+  }
+
+  .notes {
+    li {
+      &:nth-child(odd) {
+        background-color: #f2f2f2;
+      }
+
+      a {
+        display: flex;
+        padding: 3px 0;
+        font-size: 12px;
+        border: 2px solid transparent;
+      }
+
+      .router-link-exact-active {
+        border: 2px solid #81c0f3;
+        border-radius: 3px;
+      }
+
+      span {
+        padding: 0 10px;
+        flex: 1;
+      }
+    }
+  }
+}
+</style>
